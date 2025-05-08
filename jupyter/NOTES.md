895e8c2f63a857fdd7ce1dd0b2012ab6e4a0ccfd
Joseph C Wang <joequant@gmail.com>
2025-05-08 06:06:05 +0000

    add install notes

---
+++ jupyter/NOTES.md
@@ -0,0 +1,13 @@
+Uniswap simulator
+
+https://github.com/idrees535/Uniswap-V3-Simulator
+
+Install within the jupyterlab terminal
+
+Needed to use anvil rather than ganache for the blockchain backend
+
+Install anvil rather than ganache
+
+https://github.com/idrees535/Uniswap-V3-Simulator/issues/1
+
+https://medium.com/@natelapinski/run-your-own-ethereum-testnet-using-anvil-and-python-7e18c93a4315
--- jupyter/setup.sh
+++ jupyter/setup.sh
@@ -2,8 +2,15 @@
 set -e
 sudo apt install python3 python3-dev git curl
 sudo apt install npm
-sudo npm install -g ganache-cli
 sudo -E python3  ./bootstrap.py --admin admin
+
+# use anvil for backend
+# https://github.com/idrees535/Uniswap-V3-Simulator/issues/1
+
+#https://medium.com/@natelapinski/run-your-own-ethereum-testnet-using-anvil-and-python-7e18c93a4315
+
+curl -L https://foundry.paradigm.xyz | bash
+~/.foundry/bin/foundryup
 
 
 
