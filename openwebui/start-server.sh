ba3cefb87c24066b9264b47eb2a649d9dbb0f4ca
Joseph C Wang <joequant@gmail.com>
2025-07-18 11:20:18 +0000

    fix start-server

--- openwebui/start-server.sh
+++ openwebui/start-server.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-while true;
-do
-    ssh joe@dev1.hkphysics.hk -N -L 172.17.0.1:11434:localhost:11434
-done
+autossh -M 0 joe@dev1.hkphysics.hk -N -o "StrictHostKeyChecking no" -L 172.17.0.1:11434:localhost:11434 &
+autossh -M 0 joe@dev1.hkphysics.hk -N -o "StrictHostKeyChecking no" -L localhost:11434:localhost:11434 &
+
