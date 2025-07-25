eed6be121f02ad0eb4bdee85234984574aae7fbe
Joseph C Wang <joequant@gmail.com>
2025-07-25 07:04:21 +0000

    add HOST

--- openwebui/start-server.sh
+++ openwebui/start-server.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-autossh -M 0 joe@dev1.hkphysics.hk -N -o "StrictHostKeyChecking no" -L 172.17.0.1:11434:localhost:11434 &
-autossh -M 0 joe@dev1.hkphysics.hk -N -o "StrictHostKeyChecking no" -L localhost:11434:localhost:11434 &
+HOST="${HOST:-joe@dev1.hkphysics.hk}"
+
+autossh -M 0 $HOST -N -o "StrictHostKeyChecking no" -L 172.17.0.1:11434:localhost:11434 &
+autossh -M 0 $HOST -N -o "StrictHostKeyChecking no" -L localhost:11434:localhost:11434 &
 
