7dc468c88c6be5595ade9b3b1954b13e7718b8d8
Joseph C Wang <joequant@gmail.com>
2025-08-09 13:57:48 +0000

    checkpoint and add autossh.service

--- openwebui/start-server.sh
+++ openwebui/start-server.sh
@@ -2,6 +2,6 @@
 
 HOST="${HOST:-joe@dev1.hkphysics.hk}"
 
-autossh -M 0 $HOST -N -o "StrictHostKeyChecking no" -L 0.0.0.0:11434:localhost:11434 &
-autossh -M 0 $HOST -N -o "StrictHostKeyChecking no" -L 0.0.0.0:2525:localhost:25 &
+autossh -M 0 $HOST -N -o "StrictHostKeyChecking no" -L 0.0.0.0:11434:localhost:11434 -L 0.0.0.0:2525:localhost:25 &
+
 
