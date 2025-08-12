92b1326f2e50b59660259c5ee8fea749ca4230fd
Joseph C Wang <joequant@gmail.com>
2025-08-12 08:52:08 +0000

    add install.sh

--- install.sh
+++ install.sh
@@ -6,3 +6,10 @@ cp openwebui/autossh.service /etc/systemd/system
 systemctl daemon-reload
 systemctl enable caddy
 systemctl enable autossh
+
+wget https://github.com/typst/typst/releases/download/v0.12.0/typst-x86_64-unknown-linux-musl.tar.xz
+tar xf typst-x86_64-unknown-linux-musl.tar.xz
+rm -f typst*.xz
+mv typst-x86_64-unknown-linux-musl/typst /usr/bin
+chmod a+x /usr/bin/typst
+rm -rf  typst-x86_64-unknown-linux-musl
