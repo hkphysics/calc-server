f9c8861c1d69119a28a942344b7d218533f1cd3d
Joseph C Wang <joequant@gmail.com>
2026-04-07 00:01:49 +0000

    Fix autossh service path in install script

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt-get install caddy autossh
 cp caddy/Caddyfile /etc/caddy
-cp openwebui/autossh.service /etc/systemd/system
+cp etc/systemd/system/autossh.service /etc/systemd/system
 
 systemctl daemon-reload
 systemctl enable caddy
