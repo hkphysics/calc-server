64816c28a9f0253ee19c0efbc08ea85c6a489186
Joseph C Wang <joequant@gmail.com>
2026-04-18 18:29:14 +0000

    add tensorboard and disable ai mcp server

--- jupyter/setup.sh
+++ jupyter/setup.sh
@@ -45,6 +45,9 @@ apt install python3 python3-dev git curl npm texlive texlive-fonts-extra texlive
 python3  ./bootstrap.py
 cp config.yaml /opt/tljh/config
 /opt/tljh/user/bin/pip install -r requirements.txt
+
+# The mcp server interferes with port 3001
+rm /opt/tljh/user/etc/jupyter/jupyter_server_config.d/jupyter_server_mcp.json
 pushd /opt/tljh/hub/lib/python*/site-packages
 patch -p1 < $script_dir/badname.patch
 popd
