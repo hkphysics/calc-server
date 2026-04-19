c6cf57880d1cbe748fdfc828505fd77150124771
Joseph C Wang <joequant@gmail.com>
2026-04-19 14:16:47 +0000

    install pkgs

--- install-pkg.sh
+++ install-pkg.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
-apt-get install emacs git docker.io docker-compose caddy autossh docker-buildx
+apt-get install emacs git caddy autossh
 
+if [ -f /etc/debian_version ]; then
+    debver=$(< /etc/debian_version)
+    echo "Running on Debian (version $debver)"
+# Add Docker's official GPG key:
+    apt update
+    apt install ca-certificates curl
+    install -m 0755 -d /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+    chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+    tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/debian
+Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
+Components: stable
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+    apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+else
+    apt install docker.io docker-compose docker-buildx
+fi
